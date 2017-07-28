9e601f18d5aa0aa88d643e0ea4efd87824b03e01
Youngok <joylife052@gmail.com>
2017-07-28 11:27:56 +0900

    initial release

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,23 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Assignments Part2 for R-Programming Week3
+# @written by Youngok Kim, joylife052@gmail.com
+#
 
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(inv) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
@@ -12,4 +27,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    
+    if(nrow(data) == ncol(data)) { # check square matrix, or not
+        if(det(data) != 0) { # if exist inverse matrix
+            m <- solve(data, ...)
+            x$setsolve(m)
+        } else{
+            message("does not exist inverse matrix")
+        }
+    } else {
+        message("not square matrix")
+    }
+    m
 }
